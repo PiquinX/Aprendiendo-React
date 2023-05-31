0ab8df8671ba32adea2e34e765ea348fa6049e61
Santy <sanpiquin@gmail.com>
2023-05-31 12:00:24 -0300

    proyecto de click per second terminado

--- proyects/Curso React/src/App.jsx
+++ proyects/Curso React/src/App.jsx
@@ -2,7 +2,7 @@ import { TwiiterFollowingCard } from "./twitterFollowingCard";
 import './App.css'
 
 export function App (){
-    const formatUsername = username=> `@${username}`; 
+    const formatUsername = username => `@${username}`; 
 
     const midudev = {formatUsername : formatUsername, initialIsFollowing : true, userName:'midudev'};  // esto no es tan buen practica
     // se le pone initialIsFollowing porque el estado solo se inicializa una vez(la prop tiene true o false en este caso, es decir que es como si pusieramos true o false sin mas).
